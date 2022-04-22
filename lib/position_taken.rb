c6762d7df797dcf536201cf498222b256bc85702
jtsarvida <jtsarvida@gmail.com>
2022-04-22 03:37:02 -0400

    commit tree a0ab6ea4040c05c97d982c2ac94764dd7f69b176

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+    bool = nil
+    if (board[index] == " " || board[index] == ""|| board[index] == nil)
+        bool = false
+    elsif (board[index] == "X" || board[index] =="O")
+        bool = true
+    end
+    return bool
+end
